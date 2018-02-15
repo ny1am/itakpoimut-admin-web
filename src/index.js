3d9a2fa63819ae5c4b1308f5b9585ca8a1cb2e7a
ny1am <ny1am.lviv.ua@gmail.com>
2018-02-15 10:21:46 +0200

    fix: promise.prototype.finally

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
+import promiseFinally from 'promise.prototype.finally';
 import 'isomorphic-fetch';
 
 import Root from 'components/Root';
 import configureStore, { history } from 'store/configureStore';
+
+promiseFinally.shim();
 
 const store = configureStore();
 
